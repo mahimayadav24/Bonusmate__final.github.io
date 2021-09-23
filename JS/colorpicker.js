5ee0f7ce3dbf7539092c5e0b9e9dc6fdc5954bfb
mahimayadav24 <88049838+mahimayadav24@users.noreply.github.com>
2021-09-23 21:35:15 +0530

    insert

---
+++ JS/colorpicker.js
@@ -0,0 +1,32 @@
+$('#colorpicker').on('input', function () {
+    $('#hexcolor').val(this.value);
+});
+$('#hexcolor').on('input', function () {
+    $('#colorpicker').val(this.value);
+});
+
+
+
+$('#colorpickers').on('input', function () {
+    $('#hexcolors').val(this.value);
+});
+$('#hexcolors').on('input', function () {
+    $('#colorpickers').val(this.value);
+});
+
+
+$('#colorpickerss').on('input', function () {
+    $('#hexcolorss').val(this.value);
+});
+$('#hexcolorss').on('input', function () {
+    $('#colorpickerss').val(this.value);
+});
+
+
+
+$('#colorpickersr').on('input', function () {
+    $('#hexcolorsr').val(this.value);
+});
+$('#hexcolorsr').on('input', function () {
+    $('#colorpickersr').val(this.value);
+});
